5007954c6453c97ed00a786f5b70cea656625729
vsfdhyibavds <eugenco578@gmail.com>
2024-05-08 16:25:37 +0300

    functions in dart

---
+++ Functions.dart
@@ -0,0 +1,38 @@
+// Functions in Dart
+// 1. Addition
+int addTwo(int a, int b){
+    int sum = a + b;
+    return sum;
+}
+
+// 2. Subtraction
+int subtractTwo(int a, int b){
+    int subtract = a - b;
+    return subtraction;
+}
+
+// 3. Multiplication
+int multiplyTwo(a * b){
+    int multiply = a * b;
+    return multiplication;
+}
+
+// 4. Division
+int divideTwo(a / b){
+    int divide = a / b;
+    return division;
+}
+
+// 5. StringLength
+int stringLength(String str){
+    return str.Length;
+}
+
+// 6. GetFirstLength
+dynamic getFirstElement(List<dynamic> List){
+    if(List.isNotEmpty){
+        return list[0];
+    } else {
+        return null;
+    }
+}
